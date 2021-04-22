fb5bdef3657b68381271d0c1b353ccb17eb807cf
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-04-23 01:09:35 +0300

    frist try

--- QuizJS.js
+++ QuizJS.js
@@ -1,27 +1,37 @@
 
-const body=document.querySelector("body")
+const body1=document.querySelector("body")
 
 const quastion=document.createElement("div");
 quastion.innerHTML = "";
 quastion.id="div1";
-body.append(quastion);
+quastion.style.color="green";
+body1.append(quastion);
 
-const array_quastion=["q1","q2","q3","q4"];
+
+
+let array_quastion=["q1hhhhhhhhhhhhh","q2ddddddd","q3","q4","tttttttttt"];
 
  const func_quadtion=()=>{
-     if(array_quastion.length!==0)
-    const quastion_f=document.createElement("p");
-    quastion.innerHTML=array_quastion[0];
-    quastion.append(quastion_f);
-    array_quastion=array_quastion.slice(1);
 
+     if(array_quastion.length!==0){
+     document.getElementById("div1").innerHTML="";
+     const quastion_f=document.createElement("p");
+     quastion_f.innerHTML=array_quastion[0];
+     quastion_f.id="quastion_f";
+     quastion_f.style.color="red";
+     quastion.append(quastion_f);
+     array_quastion.splice(0,1)
+    // array_quastion=array_quastion.slice(1);
      }
-
+     }
 
      button1=document.createElement("button")
      button1.id="button";
-     button1.innerText="Add";
-     body.append(button1);
+     button1.innerText="Next";
+     button1.style.height="40px";
+     button1.style.width="60px";
+     body1.append(button1);
+
 
      document.getElementById("button").addEventListener("click" ,function(){
      return func_quadtion();
